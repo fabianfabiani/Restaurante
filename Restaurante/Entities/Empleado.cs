56dc0f30392e78f4f52974cff8ee06c3bb9af411
Fabian Fabiani <oeste24@hotmail.com>
2024-08-15 23:04:14 -0300

    Inicio

---
+++ Restaurante/Entities/Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Restaurante.Entities
+{
+    public class Empleado : ClaseBase
+    {
+        [ForeignKey(nameof(Sector))]
+        public Sector Sector { get; set; }
+
+        [ForeignKey(nameof(Rol))]
+        public Rol Rol { get; set; }
+
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+        public int IdSector { get; set; }
+        public int IdRol { get; set; }
+
+    }
+}
